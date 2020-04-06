f7944517a9fd9b3e1248f0298bb20a33d9577320
Meenu R <34276693+codergirlx0x0@users.noreply.github.com>
2020-04-06 10:36:38 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -4,163 +4,207 @@ var app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
 var path2 = __dirname + '/img/';
+var path3 = __dirname + '/js/';
+var path4 = __dirname + '/css/';
+const fs = require("fs");
 const mongoose = require("mongoose");
 const readline = require("readline");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
 const User = require("./user.js");
-mongoose.connect(process.env.MONGO_URL,{useNewUrlParser: true, useUnifiedTopology: true});
+const Schema = mongoose.Schema;
+const requireLogin = require("./middleware/requireLogin.js");
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(express.urlencoded({
-  extended: true
+    extended: true
 }));
 app.use(cookieParser());
 app.use(session({
-  key: "user_sid",
-  secret: "3214124312",
-  resave: true,
-  saveUninitialized: false,
-  cookie: {
-    expires: 600000
-  }
+    key: "user_sid",
+    secret: "3214124312",
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+        expires: 600000
+    }
 }));
 
 app.use((req, res, next) => {
-  if (req.cookies.user_sid && !req.session.user) {
-      res.clearCookie('user_sid');        
-  }
-  next();
+    if (req.cookies.user_sid && !req.session.user) {
+        res.clearCookie('user_sid');
+    }
+    next();
 });
 //set up readline variable
 var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
 });
 
 //exit on enter
-rl.on("line",(line)=>{
-  if (line === ""){
-    console.log("Goodbye");
-    process.exit(0);
-  }
+rl.on("line", (line) => {
+    if (line === "") {
+        console.log("Goodbye");
+        process.exit(0);
+    }
 })
-
-
-
-
-
-
 
 var db = mongoose.connection;
-db.on("error",console.error.bind(console,"connection error"));
-db.once("open",()=>{
-  console.log("successfully connected");
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+    console.log("successfully connected");
 })
 
-
-router.use(function (req,res,next) {
-  console.log("/" + req.method);
-  next();
+router.use(function (req, res, next) {
+    console.log("/" + req.method);
+    next();
 });
 
-router.get("/",function(req,res){
-  res.sendFile(path + "index.html");
+router.get("/", function (req, res) {
+    res.sendFile(path + "index.html");
 });
-router.get("/logoimg",function(req,res){});
+router.get("/cal", function (req, res) {
+    res.sendFile(path3 + "calendar.js");
+});
+router.get("/calcss", function (req, res) {
+    res.sendFile(path4 + "calendar.css");
+});
+router.get("/logoimg", function (req, res) { });
 
-router.get("/img",function(req,res){
-  res.sendFile(path2 + "logo.png");
+router.get("/img", function (req, res) {
+    res.sendFile(path2 + "logo.png");
 });
-router.get("/loginimg",function(req,res){
-  res.sendFile(path2 + "loginimg.png");
+router.get("/loginimg", function (req, res) {
+    res.sendFile(path2 + "loginimg.png");
 });
-router.get("/greytrees",function(req,res){
-  res.sendFile(path2 + "greytrees.jpg");
+router.get("/greytrees", function (req, res) {
+    res.sendFile(path2 + "greytrees.jpg");
 });
-router.get("/signupbackground",function(req,res){
-  res.sendFile(path2 + "signupbackground.jpg");
-});
-
-router.get("/about",function(req,res){
-  res.sendFile(path + "about.html");
-});
-
-router.get("/contact",function(req,res){
-  res.sendFile(path + "contact.html");
+router.get("/signupbackground", function (req, res) {
+    res.sendFile(path2 + "signupbackground.jpg");
 });
 
-router.get("/dashboard",function(req,res){
-  res.sendfile(path+"dashboard.html");
+router.get("/about", function (req, res) {
+    res.sendFile(path + "about.html");
 });
 
-router.get("/login",function(req,res){
-  res.sendFile(path + "login.html");
+router.get("/contact", function (req, res) {
+    res.sendFile(path + "contact.html");
 });
-router.get("/signup",function(req,res){
-  res.sendFile(path + "signup.html");
+
+router.get("/dashboard", requireLogin, function (req, res,next) {
+    let html = fs.readFileSync(path+"dashboard.html","utf8");
+    html = html.replace("$USER", req.session.firstname);
+    res.send(html);
 });
-router.get("/calendar",function(req,res){
-  res.sendFile(path + "calendar.html");
+
+
+router.get("/login", function (req, res) {
+    res.sendFile(path + "login.html");
 });
-router.get("/signup_success",function(req,res){
-  res.sendFile(path+"signup_success.html");
+router.get("/signup", function (req, res) {
+    res.sendFile(path + "signup.html");
+});
+router.get("/calendar", function (req, res) {
+    res.sendFile(path + "calendar.html");
+});
+router.get("/signup_success", function (req, res) {
+    res.sendFile(path + "signup_success.html");
 })
 
-
-router.get("/signup",function(req,res){
-  res.sendFile(path + "signup.html");
+router.get("/creategroup",function(req,res){
+  res.sendFile(path + "creategroup.html");
 });
 
-app.use("/",router);
-
-app.use("*",function(req,res){
-  res.sendFile(path + "404.html");
+router.get("/signup", function (req, res) {
+    res.sendFile(path + "signup.html");
 });
 
-app.listen(3000,function(){
-  console.log("Live at Port 3000");
+app.use("/", router);
+
+app.use("*", function (req, res) {
+    res.sendFile(path + "404.html");
 });
 
-router.post("/login", (req,res)=>{
-  console.log(req.body);
-  
-  User.authenticate(User, req.body.inputEmail,req.body.password, (err, user) =>{
-    if (err){
-      return err;
-    } else {
-      req.session.user = user._id;
-      res.redirect("dashboard");
-    }
-  });
+app.listen(3000, function () {
+    console.log("Live at Port 3000");
 });
 
-//registring an accoutn
-router.post("/signup",(req,res) => {
-  
-  console.log(req.body);
-  if (req.body.password[0] === req.body.password[1]){
-    console.log("password good");
-  }
-  var userData = {
-    firstname: req.body.firstname,
-    lastname: req.body.lastname,
-    email: req.body.inputEmail,
-    password: req.body.password[0]
-  }
-  User.init().then(() => {
-    User.create(userData, (err,user) => {
-      if (err){
-        if (err.code === 11000){
-          return res.status(400).json({
-            msg: "User already exists"
-          })
+router.post("/login", (req, res) => {
+    console.log(req.body);
+
+    User.authenticate(User, req.body.inputEmail, req.body.password, (err, user) => {
+        if (err) {
+            return err;
+        } else {
+            req.session.user = user._id;
+            req.session.firstname = user.firstname;
+            res.redirect("dashboard");
         }
-        console.log(err);
-      } else {
-        console.log("sent:");
-        console.log(userData);
-        return res.redirect("signup_success");
-      }
     });
-  });
+});
+
+//registring an account
+router.post("/signup", (req, res) => {
+
+    console.log(req.body);
+    if (req.body.password[0] === req.body.password[1]) {
+        console.log("password good");
+    }
+    var userData = {
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        email: req.body.inputEmail,
+        password: req.body.password[0]
+    }
+    User.init().then(() => {
+        User.create(userData, (err, user) => {
+            if (err) {
+                if (err.code === 11000) {
+                    return res.status(400).json({
+                        msg: "User already exists"
+                    })
+                }
+                console.log(err);
+            } else {
+                console.log("sent:");
+                console.log(userData);
+                return res.redirect("signup_success");
+            }
+        });
+    });
+});
+
+//Courselist Database Setup
+var ClassSchema = new Schema({
+    classname: String,
+    crn: String,
+
+});
+
+var Class = mongoose.model("Class", ClassSchema);
+
+router.post("/class", (req, res) => {
+    console.log(req.body);
+    var userData = {
+        classname: req.body.classname,
+        crn: req.body.crn,
+
+    }
+    Class.create(userData, (err, user) => {
+        if (err) {
+            console.log(err);
+        } else {
+            return res.sendStatus(200)
+        }
+    });
+});
+
+router.get("/class", function (req, res) {
+    Class.find({}, function (err, item) {
+        console.log(item);
+    })
+    return res.sendStatus(200)
 });
